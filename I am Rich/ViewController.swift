44d2a94d239e281c0178f09d58b83821c50be18c
Osman Canboğa <osmancanboga@hotmail.com>
2021-12-19 18:29:48 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by osman canboga on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
